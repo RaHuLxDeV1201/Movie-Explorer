cc5946e1593285fd78bfdbe70976aeb4402d9f4d
RaHuLxDeV1201 <priyadarshirahul945@gmail.com>
2026-07-20 14:12:01 +0530

    Initial commit: Movie Explorer dashboard complete

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/Movie-Explorer/', // Must match your exact repository name with slashes!
 })
